adc89fe78c34af1361ab7a8d3c8dc1a3d9f54530
Denise <penaredondodenise@gmail.com>
2020-06-19 18:30:38 +0800

    added files

---
+++ rhian.py
@@ -0,0 +1,1 @@
+A quick and simplified answer is that Lorem Ipsum refers to text that the DTP (Desktop Publishing) industry use as replacement text when the real text is not available. For example, when designing a brochure or book, a designer will insert Lorem ipsum text if the real text is not available.
